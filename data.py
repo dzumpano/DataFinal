9ab528a4370cd1960ebb459eba57bc1711971088
Nick <dominiczumpanolivingston@gmail.com>
2022-05-11 14:47:30 -0600

    Setup

--- data.py
+++ data.py
@@ -1,15 +1,33 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 d = pd.read_csv("TWTR.csv")
+d_main = d[::-1] # invert data frame
+d_main.set_index('Date', inplace=True)
+#d_main.index = pd.to_datetime(d_main.index).date
 
 # TODO Part 2: dim reduction, clustering, classification
-print(d.head)
 
-# plt.plot(d["Date"], d["High"], d["Low"])
-# plt.show()
-# Elon musk bought twitter 4.25
+print(d_main.head)
+
+ax = plt.gca()
+plt.plot(d["Date"], d_main["High"], label = "High")
+plt.plot(d["Date"], d_main["Low"], label = "Low")
+plt.axvline(x=242)
+plt.title("")
+plt.legend()
+plt.xlabel("Date")
+plt.ylabel("Stock Price in USD")
+ax.xaxis.set_major_locator(mdates.WeekdayLocator(interval=6))
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+plt.gcf().autofmt_xdate()
+
+plt.show()
+
+
+# Elon Musk bought twitter 4/25
 
 # Part 3:
 print(d.info())
