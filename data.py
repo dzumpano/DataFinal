448f0ccdd5dccf0c25f042e28108a6dd394fb29d
Nick <dominiczumpanolivingston@gmail.com>
2022-05-12 10:37:15 -0600

    Setup

--- data.py
+++ data.py
@@ -4,24 +4,37 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 d = pd.read_csv("TWTR.csv")
-d_main = d[::-1] # invert data frame
-d_main.set_index('Date', inplace=True)
-#d_main.index = pd.to_datetime(d_main.index).date
+d_in = d[::-1] # invert data frame
+d_main = d_in.set_index('Date')
+d_main.index = pd.to_datetime(d_main.index).date
 
 # TODO Part 2: dim reduction, clustering, classification
 
 print(d_main.head)
 
+plt.figure(0)
 ax = plt.gca()
-plt.plot(d["Date"], d_main["High"], label = "High")
-plt.plot(d["Date"], d_main["Low"], label = "Low")
+plt.plot(d_in["Date"], d_main["High"], label = "High")
+plt.plot(d_in["Date"], d_main["Low"], label = "Low")
 plt.axvline(x=242)
 plt.title("")
 plt.legend()
-plt.xlabel("Date")
+plt.xlabel("Date m-d-Y")
 plt.ylabel("Stock Price in USD")
 ax.xaxis.set_major_locator(mdates.WeekdayLocator(interval=6))
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d-%Y'))
+plt.gcf().autofmt_xdate()
+
+plt.show()
+
+plt.figure(1)
+ax = plt.gca()
+plt.plot(d_in["Date"].tail(60), d_main["Volume"].tail(60))
+plt.xlabel("Date m-d-Y")
+plt.ylabel("Stock Volume")
+plt.axvline(x=242, color = "red")
+ax.xaxis.set_major_locator(mdates.WeekdayLocator(interval=6))
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d-%Y'))
 plt.gcf().autofmt_xdate()
 
 plt.show()
