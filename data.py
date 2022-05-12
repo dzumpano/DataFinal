6388c11b6cb858dd2e3835a2236e2a2a3b395c3b
colinshoots <89663240+colinshoots@users.noreply.github.com>
2022-05-11 20:52:03 -0600

    Update data.py

--- data.py
+++ data.py
@@ -1,30 +1,38 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
+from matplotlib import pyplot as plt, dates as mdates
+from datetime import datetime as dt
+from sklearn.cluster import DBSCAN
 
-d = pd.read_csv("TWTR.csv")
-d_main = d[::-1] # invert data frame
-d_main.set_index('Date', inplace=True)
-#d_main.index = pd.to_datetime(d_main.index).date
+d = pd.read_csv("TWTR.csv", delimiter=',')
+d_main = d[::-1]  # invert data frame
+
+
+
 
 # TODO Part 2: dim reduction, clustering, classification
-
-print(d_main.head)
+dates = d_main["Date"]
+x_values = [dt.strptime(d, "%m/%d/%Y").date() for d in dates]
 
 ax = plt.gca()
-plt.plot(d["Date"], d_main["High"], label = "High")
-plt.plot(d["Date"], d_main["Low"], label = "Low")
-plt.axvline(x=242)
-plt.title("")
-plt.legend()
+plt.plot(x_values, d_main["High"], label="High")
+plt.plot(x_values, d_main["Low"], label="Low")
+
+plt.title("Date vs High and Low Price")
 plt.xlabel("Date")
 plt.ylabel("Stock Price in USD")
-ax.xaxis.set_major_locator(mdates.WeekdayLocator(interval=6))
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-plt.gcf().autofmt_xdate()
 
+formatter = mdates.DateFormatter("%Y-%m-%d")
+ax.xaxis.set_major_formatter(formatter)
+locator = mdates.WeekdayLocator()
+ax.xaxis.set_major_locator(locator)
+plt.gcf().autofmt_xdate()
+datemin = dt(2021, 5, 9)
+datemax = dt(2022, 5, 6)
+plt.legend()
+ax.set_xlim(datemin, datemax)
 plt.show()
+
 
 
 # Elon Musk bought twitter 4/25
@@ -38,5 +46,3 @@ print("We removed earlier data that was non-essential")
 # TODO Unexpected Results?
 # TODO How will the work help understand the problem
 # TODO If you had more time?
-
-
